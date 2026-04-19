f3e3c7101801d2a14a8b9796b162a7ed2ff74ab6
Dharini13 <dharini.km@gmail.com>
2026-04-19 17:06:29 +0000

    fixed pipeline

---
+++ upload_dataset.py
@@ -0,0 +1,20 @@
+from huggingface_hub import login, upload_file
+import os
+
+login()
+
+upload_file(
+    path_or_fileobj="tourism-mlops/data/train_data.csv",
+    path_in_repo="train_data.csv",
+    repo_id="Dharini95/tourism-package-dataset",
+    repo_type="dataset"
+)
+
+upload_file(
+    path_or_fileobj="tourism-mlops/data/test_data.csv",
+    path_in_repo="test_data.csv",
+    repo_id="Dharini95/tourism-package-dataset",
+    repo_type="dataset"
+)
+
+print("Dataset uploaded")
